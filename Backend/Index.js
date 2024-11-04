c89eb929498e90b78d0a36554f01232d8627aa1c
Mehulk473 <165556891+Mehulk473@users.noreply.github.com>
2024-11-04 11:41:21 +0530

    Backend file updated 4 nov

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
+const express = require("express")
+const app = express()
+const PORT = 3000
 
-helo i m Aryan
->>>>>>> bb5cb0cc54c7cbc654d307544e55486137d96aeb
-sdsdwsdsccscs
 
-sonu verm axsxx
->>>>>>> 0465dd194f2fe8a8ed2000f7d588aafe92f17f2c
->>>>>>> efb007e5df6328ef1eba54c8da545d2573f01051
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+app.listen(PORT,()=>{
+    console.log("Server is running at PORT - 3000")
+})
